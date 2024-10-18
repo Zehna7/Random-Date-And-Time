c2d3d3ecd7d6aeb2e852f77cfb0cde75eba950c2
Zehna7 <zehna.omar2012@gmail.com>
2024-10-18 17:29:33 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+import time
+
+def getRandomDate(starDate, endDate):
+    print("Printing random date between", starDate, "and", endDate)
+    randomGenerator = random.random()
+    dateFormat = "%d/%m/%Y"
+    starttime = time.mktime(time.strptime(starDate, dateFormat))
+    endtime = time.mktime(time.strptime(endDate, dateFormat))
+    randomTime = starttime + randomGenerator * (endtime - starttime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+print("Random Date = ", getRandomDate("1/1/2020", "12/12/2024"))
